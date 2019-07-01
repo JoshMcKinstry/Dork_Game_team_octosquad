f92ef38a2c500060a4231acc382e8e8972a4258d
Sinh Mai <sinhmai0@gmail.com>
2019-06-30 22:51:11 -0600

    updated methods in the items class

--- Features/items.py
+++ Features/items.py
@@ -1,16 +1,14 @@
 class Items:
-    """ Creates item objects"""
+    """ Creates, uses and stores item objects"""
 
-    def __init__(self, item, use):
+    def __init__(self, item):
         self.item = item
-        self.use = use
 
     def add_item(self, item):
         self.item.append(item)
     
     def use(self, item):
-        self.print("You used the " + item)
+        print("You used the " + item)
 
     def store(self, item,):
-        self.print("You stored the " + item + " into your inventory")
-        
+        print("You stored the " + item + " into your inventory")
