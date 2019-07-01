f3d287e0902db10684acad5c8be0d4fbff3eeec5
Sinh Mai <sinhmai0@gmail.com>
2019-06-30 22:45:38 -0600

    added an add_item and use method to the item class

--- Features/items.py
+++ Features/items.py
@@ -1,6 +1,13 @@
 class Items:
     """ Creates item objects"""
 
-    def __init__(self, name, use):
-        self.name = name
+    def __init__(self, item, use):
+        self.item = item
         self.use = use
+
+    def add_item(self, item):
+        self.item.append(item)
+    
+    def use(self, item):
+        self.print("You used the " + item)
+        
