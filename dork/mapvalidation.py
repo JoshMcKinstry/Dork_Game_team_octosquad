dc66ef03b61173dfbdf4426f75813ce782bd02f9
Andres Restrepo <51206864+bourbaki95@users.noreply.github.com>
2019-07-12 08:52:53 -0600

    Update mapvalidation.py
    Pair Programming with Warren Ramos.

--- dork/mapvalidation.py
+++ dork/mapvalidation.py
@@ -1,45 +1,87 @@
 """
-A class that validates a maze coming from a .yml/.yaml file
+A module that loads and validate the game data.
 """
 
-
-def load_rooms(maze):
+def load_rooms(data):
     """
     Loading the room names
     """
-    for room_name in maze:
-        room_names = list(maze[room_name].keys())
-    del room_names[-1]
+    room_names = list(data['Rooms'].keys())
     return room_names
 
 
-def load_cardinals(maze):
+def load_cardinals(data, room_names):
     """
     Loading the room cardinals
     """
     room_cardinals = []
-    for room_name in maze:
-        room_specs = list(maze[room_name].values())
-    scope = range(len(room_specs)-1)
-    for i in scope:
-        neighbors_dict = room_specs[i]['Neighbors']
-        room_cardinals.append(neighbors_dict)
+    for name in room_names:
+        cardinal = data['Rooms'][name]['Neighbors']
+        room_cardinals.append(cardinal)
     return room_cardinals
 
 
-def load_items(maze):
+def load_doors(data, room_names):
     """
-    Loading the room items
+    Loading the doors in each room
     """
-    room_items = []
-    for room_name in maze:
-        room_specs = list(maze[room_name].values())
-    scope = range(len(room_specs)-1)
-    for i in scope:
-        items_dict = room_specs[i]['Items']
-        room_items.append(items_dict)
-    return room_items
+    room_doors = []
+    for name in room_names:
+        door = data['Rooms'][name]['Door']
+        room_doors.append(door)
+    return room_doors
 
+
+def load_room_descrips(data, room_names):
+    """
+    Loading each room respective main description
+    """
+    room_descrips = []
+    for name in room_names:
+        descrip = data['Rooms'][name]['Description']
+        room_descrips.append(descrip)
+    return room_descrips
+
+
+def load_list_room_items(data, room_names):
+    """
+    Loading each room respective list of items
+    """
+    items_list = []
+    for name in room_names:
+        item_list = data['Rooms'][name]['Items']
+        items_list.append(item_list)
+    return items_list
+
+
+def load_items(data):
+    """
+    Loading items into the game 
+    """
+    items_name = list(data['Items'].keys())
+    return items_name
+
+
+def load_items_descriptions(data, items_name):
+    """
+    Loading each item respective description
+    """
+    item_descriptions = []
+    for name in items_name:
+        description = data['Items'][name]['Description']
+        item_descriptions.append(description)
+    return item_descriptions
+
+
+def load_items_properties(data, items_name):
+    """
+    Loading each item respective properties
+    """
+    item_properties = []
+    for name in items_name:
+        attribute = data['Items'][name]['Properties']
+        item_properties.append(attribute)
+    return item_properties
 
 def check_rooms(room_names):
     """
@@ -60,6 +102,18 @@ def check_cardinals(room_cardinals):
             invalid_cardinal = True
             break
     return invalid_cardinal
+
+
+def no_null_cardinal(room_cardinals):
+    """
+    Removing null cardinals for validation
+    """
+    scope = range(len(room_cardinals))
+    for i in scope:
+        adjacent_rooms = list(room_cardinals[i].values())
+        adjacent_rooms[:] = (
+            room for room in adjacent_rooms if room is not None)
+    return adjacent_rooms
 
 
 def check_connections(room_names, room_cardinals):
