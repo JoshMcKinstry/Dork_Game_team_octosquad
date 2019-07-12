15e152ecb979217b544f82e295ce7f8daf17fad9
Andres Restrepo <51206864+bourbaki95@users.noreply.github.com>
2019-07-12 12:52:04 -0600

    Update game_engine.py

--- dork/game_engine.py
+++ dork/game_engine.py
@@ -1,40 +1,47 @@
 """
 A module that works as an interface between the main classes in the game
 """
-import incoming_data as ymal_data
+import incoming_data as game_data
+import item_manager as item_m
+import room_manager as room_m
+import yamlreader as reader
 
 
-def building_map(data):
+def loading_map(data):
     """
-    Map classes uses this method to build map information.
     """
-    # building_map(data)
-    room_names = ymal_data.load_rooms(data)
-    room_cardinals = ymal_data.load_cardinals(data, room_names)
-    room_doors = ymal_data.load_doors(data, room_names)
-    room_descrips = ymal_data.load_room_descrips(data, room_names)
-    room_items = ymal_data.load_list_room_items(data, room_names )
-    #We will pass this to the Map Class and then Map will pass it to the room class.
-    print('The map has been successfully loaded')
+    names = game_data.load_rooms(data)
+    neighbors = game_data.load_cardinals(data, names)
+    doors = game_data.load_doors(data, names)
+    descriptions = game_data.load_room_descrips(data, names)
+    items = game_data.load_list_room_items(data, names )
+    room_m.assembling_rooms(names, neighbors, doors, items)
+    room_m.assembling_descriptions(names, descriptions)
 
 def loading_item(data):
     """
     """
-    # loading_items(data)
-    items_name= ymal_data.load_items(data)
-    items_descriptions = ymal_data.load_items_descriptions(data, items_name)
-    items_properties = ymal_data.load_items_properties(data, items_name)
-    #We will pass this to the Item Manager and the Item Manager will pass it to Item Class
+    names= game_data.load_items(data)
+    descriptions = game_data.load_items_descriptions(data, names)
+    properties = game_data.load_items_properties(data, names)
+    item_m.assembling_items(names, descriptions, properties)
 
-def move(cardinal):
-    """
-    
-    """
+def loading_current_room():
     pass
+  
+    
+def move(cardinal, name):
+    """
+    """
+    print(room_m.move(cardinal,name))
+    
 
 
 def use_item(item_name, action):
-    """
-    
-    """
     pass
+
+if __name__ == "__main__":
+    data = reader.reading_yml('C:\Python Scripts\Yaml Loader\saved_progress.yml')
+    loading_map(data)
+    loading_item(data)
+    move('North', 'Dean\'s Office')
