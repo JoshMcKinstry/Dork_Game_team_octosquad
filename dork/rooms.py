1d3a9124fbb371b0bc66c98c3c6bec5106f763b4
Andres Restrepo <51206864+bourbaki95@users.noreply.github.com>
2019-07-12 08:49:25 -0600

    Create rooms.py
    Pair Programming with Warren Ramos.

---
+++ dork/rooms.py
@@ -0,0 +1,76 @@
+'''
+A room module that creates an abstract representation of a room
+'''
+
+class Room():
+    '''
+    A class that models an room object
+    '''
+    def __init__(self, name, neighbors, door, items):
+        self.name = name
+        self.neighbors = neighbors
+        self.door = door
+        self.items = items
+
+
+    def has_door_at(self, cardinal):
+        if self.door == None:
+            return False
+        else:
+            return True
+
+
+    def has_item(self, item_name):
+        return item_name in self.items
+
+
+    def has_neighbor(self, cardinal):
+        return self.neighbors[cardinal] != None 
+
+
+    def has_closed_door(self, cardinal):
+        return self.door['State'] == 'Closed'
+
+
+    def get_door_status(self, cardinal):
+        if self.has_door_at(cardinal):
+            return self.door['Status']
+        else:
+            return None
+
+
+    def delete_item(self, item_name):
+        if self.has_item(item_name):
+            self.items.remove(item_name)
+            return item_name + ' has been picked up from ' +  self.name
+        else:
+            return item_name + 'is not in ' + self.name
+
+
+    def add_item(self, item_name):
+        self.items.append(item_name)
+        return item_name + ' has been dropped in ' +  self.name
+
+
+if __name__ == "__main__":
+    name = 'Entrance'
+    neighbors = {'East': 'Hallway',
+      'North': None,
+      'South': None,
+      'West': 'Trail'}
+    doors = None
+    items = ['Donut', 'Paper']
+
+    room = Room(name, neighbors, doors, items)
+
+    print(room.has_door_at('East'))
+    print(room.has_item('Donut'))
+    print(room.has_item('Badge'))
+    print(room.has_neighbor('North'))
+    print(room.has_neighbor('West'))
+    print(room.has_closed_door('East'))
+    print(room.add_item('Cage'))
+    print(room.items)
+    print(room.delete_item('Cage'))
+    print(room.items)
+    print(room.get_door_status('East'))
