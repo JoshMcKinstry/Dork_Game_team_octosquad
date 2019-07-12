bf7bc248b8f3107fd1cecef85d7a8f053df01c91
Andres Restrepo <51206864+bourbaki95@users.noreply.github.com>
2019-07-12 12:55:12 -0600

    Create item_manager.py

---
+++ dork/item_manager.py
@@ -0,0 +1,12 @@
+A module that handles the functionality of items
+"""
+from items import Item
+dict_items ={ }
+
+def assembling_items(names, descriptions, properties):
+    """
+    """
+    scope = range(len(names))
+    for i in scope:
+        item = Item(names[i], descriptions[i], properties[i])
+        dict_items.update({item.name: item})
