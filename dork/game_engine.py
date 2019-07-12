160999252227a98d0320726696d3b5a7d7c11f8e
WarrenJRamos <wramos1@msudenver.edu>
2019-07-12 13:16:21 -0600

    Added documentation to game_engine (more documentation necessary when other classes have been created)

--- dork/game_engine.py
+++ dork/game_engine.py
@@ -9,6 +9,8 @@ import yamlreader as reader
 
 def loading_map(data):
     """
+    Loads the map of the game. All the rooms are created along with their
+    neighboring rooms, doors, descriptions, and items.
     """
     names = game_data.load_rooms(data)
     neighbors = game_data.load_cardinals(data, names)
@@ -20,6 +22,7 @@ def loading_map(data):
 
 def loading_item(data):
     """
+    Loads all the items available for the game.
     """
     names= game_data.load_items(data)
     descriptions = game_data.load_items_descriptions(data, names)
@@ -27,18 +30,26 @@ def loading_item(data):
     item_m.assembling_items(names, descriptions, properties)
 
 def loading_current_room():
+    """
+    Loads the current room of the player.
+    """
     pass
   
     
 def move(cardinal, name):
     """
+    Moves the player from one room to another if room exists and door is
+    open.
     """
     print(room_m.move(cardinal,name))
     
-
-
 def use_item(item_name, action):
+    """
+    Anytime a player uses an item, the action and the item the action is
+    executed on is called here.
+    """
     pass
+
 
 if __name__ == "__main__":
     data = reader.reading_yml('C:\Python Scripts\Yaml Loader\saved_progress.yml')
