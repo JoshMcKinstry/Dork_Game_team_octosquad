6294e14deb7ac28bf7858c779b583a21a5be6223
kmorale4 <kmorale4@msudenver.edu>
2019-07-14 16:20:30 -0600

    added test__init to test constructor test passed

--- dork/test_character.py
+++ dork/test_character.py
@@ -3,3 +3,17 @@ A test for character
 """
 import unittest
 import character as character
+from character import Character
+
+def test_init_method():
+    name = 'Hallway'
+    position = 'Entrance'
+    inventory =['Paper', 'Cage', 'Freshman Badge']
+    character = Character(name, position, inventory) 
+    assert character.name == name
+    assert character.position == position
+    assert character.inventory == inventory
+
+'''def test_has_item():'''
+
+    
