1b0c3a82b8a5d317a62178348d0cf27b60437c6e
Tian Liu <dantanxiaotian@gmail.com>
2019-07-14 16:04:01 -0600

    create tests for item class

--- dork/test_items.py
+++ dork/test_items.py
@@ -1,5 +1,27 @@
 """
 A test for items
 """
-import unittest
-import items as items
+from items import Item
+
+def test_init_method():
+    """
+    Testing the constructor
+    """
+    name = 'Donut'
+    description = {'This is an old fasion donut'}
+    properties = {'eatable'}
+    item = Item(name, description, properties)
+    assert item.name == name
+    assert item.description == description
+    assert item.properties == properties
+
+def test_has_property():
+    """
+    Testing the has_property method
+    """
+    name = 'Donut'
+    description = {'This is an old fasion donut'}
+    properties = ['eatable', 'pickable']
+    item = Item(name, description, properties)
+    assert item.has_property('eatable') is True
+    
