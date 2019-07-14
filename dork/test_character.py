e4071ddb11d2e003164321bd3c9795f293b98051
WarrenJRamos <wramos1@msudenver.edu>
2019-07-14 12:12:51 -0600

    Created 6 test files for these classes. (Pair programming with Andres)

---
+++ dork/test_character.py
@@ -0,0 +1,5 @@
+"""
+A test for character
+"""
+import unittest
+import dork.character as character
---
+++ dork/test_item_manager.py
@@ -0,0 +1,5 @@
+"""
+A test for item_manager
+"""
+import unittest
+import dork.item_manager as item_m
