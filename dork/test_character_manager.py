9a0ad9bfbb19c933ce5ae8e8ae1e96c9e563eab4
Tian Liu <dantanxiaotian@gmail.com>
2019-07-14 14:01:05 -0600

    testing construtor of character manager

--- dork/test_character_manager.py
+++ dork/test_character_manager.py
@@ -1,5 +1,33 @@
 """
 A test for character_manager
 """
-import unittest
+
 import character_manager as character_m
+
+def test_assembling_player():
+    """
+    Test for assembling player
+    """
+    list_position = ['Entrance']
+    list_inventory = ['donut']
+    character_m.assembling_player(list_position, list_inventory)
+    assert character_m.DICT_CHARACTERS['Player'].position == ['Entrance']
+    assert character_m.DICT_CHARACTERS['Player'].inventory == ['donut']
+    
+
+def test_player_position():
+    """
+    Test player position
+    """
+    list_position = ['Entrance']
+    list_inventory = ['donut']
+    character_m.assembling_player(list_position, list_inventory)
+    pass
+
+def test_update_player_position():
+    """
+    Test updated player position
+    """
+    list_position = ['Entrance']
+    list_inventory = ['donut']
+    pass
--- dork/test_game_engine.py
+++ dork/test_game_engine.py
@@ -1,5 +1,4 @@
 """
 A test for game_engine
 """
-import unittest
-import game_engine as game_engine
+
