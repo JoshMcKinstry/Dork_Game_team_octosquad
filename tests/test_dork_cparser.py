c6be66a43b934799a7decc3dc4c9cad488f708d9
Josh_McKinstry <jbmckinstry@live.com>
2019-07-15 08:41:42 -0600

    cleaned class import

--- tests/test_dork_cparser.py
+++ tests/test_dork_cparser.py
@@ -4,15 +4,15 @@ from types import FunctionType
 from unittest import mock
 from unittest.mock import patch
 import pytest
-from dork import commandsparser
+import dork.commandsparser as parser
 
 
 def test_repl_exists():
     """the dork repl should exist
     """
     expect = "Dork.commandsparser should define a repl method"
-    assert "repl" in vars(commandsparser), expect
-    assert isinstance(commandsparser.repl, FunctionType)
+    assert "repl" in vars(parser), expect
+    assert isinstance(parser.repl, FunctionType)
 
 
 @patch('dork.commandsparser.read', return_value='say goodbye')
@@ -22,7 +22,7 @@ def test_repl_runs_with_input(_mock_read, _mock_evaluate):
     """
     # https://forum.learncodethehardway.com/t/testing-input-and-print/1757/10
     with patch('sys.stdout') as mock_print:
-        commandsparser.repl()
+        parser.repl()
     mock_print.assert_has_calls([
         mock.call.write("Welcome to Dork!"),
         mock.call.write("\n"),
@@ -37,10 +37,10 @@ def test_move_north():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a _move_north method"
-    assert "move_north" in vars(commandsparser), expect
-    assert isinstance(commandsparser.move_north, FunctionType)
+    assert "move_north" in vars(parser), expect
+    assert isinstance(parser.move_north, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.move_north()
+        parser.move_north()
     mock_print.assert_has_calls([
         mock.call.write('You enter the boss room.'),
         mock.call.write("\n")
@@ -51,10 +51,10 @@ def test_move_south():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a _move_north method"
-    assert "move_south" in vars(commandsparser), expect
-    assert isinstance(commandsparser.move_south, FunctionType)
+    assert "move_south" in vars(parser), expect
+    assert isinstance(parser.move_south, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.move_south()
+        parser.move_south()
     mock_print.assert_has_calls([
         mock.call.write("You cannot go/move south."),
         mock.call.write("\n")
@@ -65,10 +65,10 @@ def test_move_east():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a move_east method"
-    assert "move_east" in vars(commandsparser), expect
-    assert isinstance(commandsparser.move_east, FunctionType)
+    assert "move_east" in vars(parser), expect
+    assert isinstance(parser.move_east, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.move_east()
+        parser.move_east()
     mock_print.assert_has_calls([
         mock.call.write("There is a table in the room with a donut on top."),
         mock.call.write("\n")
@@ -79,10 +79,10 @@ def test_move_west():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a move_west method"
-    assert "move_west" in vars(commandsparser), expect
-    assert isinstance(commandsparser.move_west, FunctionType)
+    assert "move_west" in vars(parser), expect
+    assert isinstance(parser.move_west, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.move_west()
+        parser.move_west()
     mock_print.assert_has_calls([
         mock.call.write("There is a beautiful garden with the roadrunner's" +
                         " nest right \nin the center of the garden." +
@@ -96,10 +96,10 @@ def test_look_north():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a look_north method"
-    assert "look_north" in vars(commandsparser), expect
-    assert isinstance(commandsparser.look_north, FunctionType)
+    assert "look_north" in vars(parser), expect
+    assert isinstance(parser.look_north, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.look_north()
+        parser.look_north()
     mock_print.assert_has_calls([
         mock.call.write("There is a door with a sign that says DANGER."),
         mock.call.write("\n")
@@ -110,10 +110,10 @@ def test_look_south():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a look_south method"
-    assert "look_south" in vars(commandsparser), expect
-    assert isinstance(commandsparser.look_south, FunctionType)
+    assert "look_south" in vars(parser), expect
+    assert isinstance(parser.look_south, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.look_south()
+        parser.look_south()
     mock_print.assert_has_calls([
         mock.call.write("There is bag of bird food with all sorts of " +
                         "insects that roadrunners love to eat."),
@@ -125,10 +125,10 @@ def test_look_east():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a look_east method"
-    assert "look_east" in vars(commandsparser), expect
-    assert isinstance(commandsparser.look_east, FunctionType)
+    assert "look_east" in vars(parser), expect
+    assert isinstance(parser.look_east, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.look_east()
+        parser.look_east()
     mock_print.assert_has_calls([
         mock.call.write("To the East, there is a sign that says Lounge."),
         mock.call.write("\n")
@@ -139,10 +139,10 @@ def test_look_west():
     """the commandsparser should ask room_printing to print
     """
     expect = "Dork.commandsparser should define a look_west method"
-    assert "look_west" in vars(commandsparser), expect
-    assert isinstance(commandsparser.look_west, FunctionType)
+    assert "look_west" in vars(parser), expect
+    assert isinstance(parser.look_west, FunctionType)
     with patch('sys.stdout') as mock_print:
-        commandsparser.look_west()
+        parser.look_west()
     mock_print.assert_has_calls([
         mock.call.write("To the West, there is a sign that says " +
                         "Roadrunner's Nest"),
@@ -154,8 +154,8 @@ def test_read_exists():
     """the commandsparser.read should exist
     """
     expect = "Dork.commandsparser should define a read method"
-    assert "read" in vars(commandsparser), expect
-    assert isinstance(commandsparser.read, FunctionType)
+    assert "read" in vars(parser), expect
+    assert isinstance(parser.read, FunctionType)
 
 
 @pytest.mark.parametrize("expected, actual", [
@@ -167,15 +167,15 @@ def test_read_takes_any_input(expected, actual):
     """the repl read function should accept any input
     """
     with patch('builtins.input', return_value=actual, autospec=True):
-        assert commandsparser.read() == expected
+        assert parser.read() == expected
 
 
 def test_evaluate_exists():
     """the commandsparser.evaluate should exist
     """
     expect = "Dork.commandsparser should define an evaluate method"
-    assert "evaluate" in vars(commandsparser), expect
-    assert isinstance(commandsparser.evaluate, FunctionType)
+    assert "evaluate" in vars(parser), expect
+    assert isinstance(parser.evaluate, FunctionType)
 
 
 @pytest.mark.parametrize("command,response", [
@@ -187,7 +187,7 @@ def test_evaluate_exists():
 def test_evaluate_go_commands(command, response):
     """dork go commands should be correctly evaluated
     """
-    assert commandsparser.evaluate(command) == response
+    assert parser.evaluate(command) == response
 
 
 @pytest.mark.parametrize("command,response", [
@@ -199,7 +199,7 @@ def test_evaluate_go_commands(command, response):
 def test_evaluate_action_commands(command, response):
     """dork action commands should be correctly evaluated
     """
-    assert commandsparser.evaluate(command) == response
+    assert parser.evaluate(command) == response
 
 
 @pytest.mark.parametrize("command,response", [
@@ -209,7 +209,7 @@ def test_evaluate_action_commands(command, response):
 def test_evaluate_starting_and_ending_dork(command, response):
     """dork game commands should be correctly evaluated
     """
-    assert commandsparser.evaluate(command) == response
+    assert parser.evaluate(command) == response
 
 
 @pytest.mark.parametrize("command,response", [
@@ -223,7 +223,7 @@ def test_evaluate_starting_and_ending_dork(command, response):
 def test_evaluate_ignores_case(command, response):
     """dork game commands should not be case sensitive
     """
-    assert commandsparser.evaluate(command) == response
+    assert parser.evaluate(command) == response
 
 
 @pytest.mark.parametrize("command,response", [
@@ -233,15 +233,15 @@ def test_evaluate_ignores_case(command, response):
 def test_evaluate_has_unknown_commands(command, response):
     """dork game has commands that are unknown
     """
-    assert commandsparser.evaluate(command) == response
+    assert parser.evaluate(command) == response
 
 
 # https://medium.com/opsops/how-to-test-if-name-main-1928367290cb
 def test_init():
     """testing of the if __name__ == "__main__" method
     """
-    with patch.object(commandsparser, "repl", return_value=42):
-        with patch.object(commandsparser, "__name__", "__main__"):
-            with patch.object(commandsparser.sys, 'exit') as mock_exit:
-                commandsparser.init()
+    with patch.object(parser, "repl", return_value=42):
+        with patch.object(parser, "__name__", "__main__"):
+            with patch.object(parser.sys, 'exit') as mock_exit:
+                parser.init()
                 assert mock_exit.call_args[0][0] == 42
