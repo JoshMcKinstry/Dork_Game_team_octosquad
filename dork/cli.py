16fe435bbcc4b3fa4f359272ed2e447663868415
WarrenJRamos <wramos1@msudenver.edu>
2019-07-15 12:15:52 -0600

    Removed a few files we don't need and fixed main method in dork module

--- dork/cli.py
+++ dork/cli.py
@@ -8,14 +8,8 @@ __all__ = ["main"]
 
 
 def main(*args):
-    """Main CLI runner for Dork
-    """
-    script_name = args[0] if args else '???'
-    if "-h" in args or '--help' in args:
-        print("usage:", script_name, "[-h]")
-    else:
-        print(*args)
     main_game_engine()
+
     # command_manage = CommandManager()
     # while command_manage.check_game_over() is not True:
     #    command_manage.read_command()
