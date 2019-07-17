49c3e095f1acbe7d2d311e7175b39f8a5b5ee0b5
WarrenJRamos <wramos1@msudenver.edu>
2019-07-17 13:09:31 -0600

    (Pair Program with RACHEL) Tested a few print statements in cli

--- tests/test_dork_cparser.py
+++ tests/test_dork_cparser.py
@@ -40,3 +40,36 @@ def test_evaluate_exists():
     expect = "Dork.commandsparser should define an evaluate method"
     assert "evaluate" in vars(parser), expect
     assert isinstance(parser.evaluate, FunctionType)
+
+
+def test_print_load(run):
+    output, _, _ = run(parser._print_load)
+    assert output == ("Select a save game and hit enter to start!\n")
+
+
+def test_menu_evaluate(run):
+    """
+    Test the menu evaluate method
+    """
+    output, _, _ = run(parser._menu_evaluate, ['help'])
+    assert output == ("Main Menu Commands for Dork\n" +
+        "help - Print a list of commands.\n" +
+        "load - Load a game save file from available saves.\n" +
+        "new - Start a new game on a fresh save file.\n" +
+        "quit - Exits the game of 'Dork'.\n")
+    output, _, _ = run(parser._menu_evaluate, ['new'])
+    assert output == ("\nStarting the game of 'Dork'.\n\n" + 
+        "Game Successfully Loaded.\n" + "**Entrance**\n" +
+        "Welcome the campus entrance! You are in the student success building. " +
+        "There are walls on the north and south ends of the " + 
+        "room. There is a door in the east and a trail in the west.\n" +
+        "You notice the following items--- Paper, Cage, Freshman-Badge.\n")
+    output, _, _ = run(parser._menu_evaluate, ['impossible'])
+    assert output == ("Please input a valid command!\nTry 'help' for more options.\n")
+
+# Got from https://stackoverflow.com/questions/15672151/is-it-possible-for-a-unit-test-to-assert-that-a-method-calls-sys-exit
+#def test_quit_dork(run):
+#    with assertRaises(SystemExit):
+#        output, _, _ = run(parser._quit_dork)
+#        assert output == "Leaving Dork...\n\n"
+    
