c8f5ab8bdc27a2bbf031caf43e8950a4c28d09fd
Andres Restrepo <arestre1@msudenver.edu>
2019-07-23 11:20:00 -0600

    Refactor in test_yaml_loader.

--- tests/test_yamlloader.py
+++ tests/test_yamlloader.py
@@ -1,19 +1,16 @@
 """A testing class for yamlloader"""
 import unittest
-from unittest.mock import patch
 import dork.yamlloader as loader
 
 
-class TestYamlReader(unittest.TestCase):
+class TestYamlLoader(unittest.TestCase):
     """
-    Testing YamlReader
+    Testing YamlLoader
     """
-    my_data = 'data'
-    @patch('yaml.safe_dump', return_value=my_data)
-    def test_yaml_loader(self, my_data):
+    def test_yaml_loader(self):
         """
         Testing the writing_yml method
         """
-        my_list = my_data
-        file_path = 'testing_out_put_file'
-        self.assertEqual(loader.writing_yml(my_list, file_path), None)
+        data = ['This a .yml file created by the yaml_loader testing module']
+        file_path = './tests/testing_files/testing_yaml_loader_file.yml'
+        self.assertEqual(loader.writing_yml(data, file_path), None)
